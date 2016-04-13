b5f121156393df79e91d0c1f33d7d13ec0710d71
Imran-hossain <imran75442@gmail.com>
2016-04-14 00:13:35 +0600

    Upload
    1st time uploading all the files

---
+++ app/Mother.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mother extends Model
+{
+	protected $table = 'mothers';
+
+	protected $fillable = [
+		'id',
+		'address',
+		'latitude',
+		'longitude',
+		'district',
+		'division',
+		'nIdNumber',
+		'alt_nc_id',
+		'nc_holders_name',
+		'nc_holders_phone',
+		'no_of_children',
+		'days_pregnant',
+		'blood_group',
+		'weight',
+		'picture'
+	];
+}
